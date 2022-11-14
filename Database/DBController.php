86bccf07c03e6f10393df955bdb7a4e333c13abb
Erika AX <eamastalxochimitl@gmail.com>
2022-11-13 19:38:20 -0600

    Added database controller

---
+++ Database/DBController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SimpleAPI\Database;
+use PDO;
+
+class DBController
+{
+    private static function connect()
+    {
+        $host = $_ENV["DB_HOST"];
+        $name = $_ENV["DB_NAME"];
+        $user = $_ENV["DB_USERNAME"];
+        $pass = $_ENV["DB_PASSWORD"];
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$name", $user, $pass);
+            $conn->exec("set names utf8mb4");
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+
+        return $conn;
+    }
+
+    public static function query(string $query)
+    {
+        $stmt = self::connect()->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+}
+
+?>
