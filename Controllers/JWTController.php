208ce5f59e8590bdc8a3e9ae9730b0ab5bfedf76
Erika AX <eamastalxochimitl@gmail.com>
2022-11-17 15:16:13 -0600

    Check and update token functions created

--- Controllers/JWTController.php
+++ Controllers/JWTController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace SimpleAPI\Controllers;
+
+use SimpleAPI\Database\DBController;
 use Firebase\JWT\JWT;
 
 class JWTController
 {
-    public static function Token($data)
+    public static function CreateToken($data)
     {
         $time = time();
         $exp = $time + 60 * 60 * 24;
@@ -20,6 +22,29 @@ class JWTController
 
         return [$jwt, $exp];
     }
-}
 
-?>
+    public static function CheckToken($token)
+    {
+        $query = "SELECT token_user,token_exp_user FROM users WHERE token_user = '$token'";
+        $token = DBController::query($query);
+
+        if (!empty($token)) {
+            $time = time();
+
+            if ($token[0]->token_exp_user > $time) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static function UpdateToken(array $token, int $id)
+    {
+        $set = "token_user = '$token[0]', token_exp_user = '$token[1]'";
+        $update = "UPDATE users SET $set WHERE id_user = $id";
+        DBController::query($update);
+    }
+}
